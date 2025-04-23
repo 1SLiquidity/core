c01ab21386a75428a5e8f2fca26da600e5303aa1
immaxkent <zmkd@solacelabs.org>
2025-04-23 23:58:26 +0100

    feat(contracts): draft for StreamDaemon complete
    summary: this contract is responsible for returning DEX's with high volume given a token pair and utilising that data feed to generate a sweet spot for the number of streams given any trade entry. This commit features a barebones contract, in line with the arch diagram, which is designed to achieve this. It is a rough, untested draft. The next step requires specific variables, getters and setters for the contract to be written. Post that, testing interactions through a fork or similar will provide for suitable integration tests.

--- src/StreamDaemon.sol
+++ src/StreamDaemon.sol
@@ -1,6 +1,68 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract StreamDaemon {
+import {IReserveFetcher} from "./interfaces/IReserveFetcher.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract StreamDaemon is Ownable {
+    
+    IReserveFetcher public reserveFetcher;
+    address[] public dexs;
+
+    constructor(address _reserveFetcher, address[] memory _dexs) Ownable(msg.sender) {
+        reserveFetcher = IReserveFetcher(_reserveFetcher);
+        for (uint256 i = 0; i < _dexs.length; i++) {
+            dexs.push(_dexs[i]);
+        }
+    }
+
+    function registerDex(address _fetcher) external onlyOwner {
+        dexs.push(_fetcher);
+    }
+
+    function returnSweetSpotAndDex(address tokenId, address tokenOut, uint256 volume, uint256 effectiveGas) public view returns (uint256 sweetSpot, address bestFetcher) {
+        (address identifiedFetcher, uint256 maxReserve) = findHighestReservesForTokenPair(tokenId, tokenOut);
+        bestFetcher = identifiedFetcher;
+        sweetSpot = _sweetSpotAlgo(volume, maxReserve, effectiveGas);
+    }
+
+    /**
+     * @dev always written in terms of 
+     *  **the token that is being added to the pool** (tokenIn)
+     */
+
+    function findHighestReservesForTokenPair(
+        address tokenIn,
+        address tokenOut
+    ) public view returns (address bestFetcher, uint256 maxReserve) {
+
+        for (uint256 i = 0; i < dexs.length; i++) {
+            IReserveFetcher fetcher = IReserveFetcher(dexs[i]);
+            (uint256 reserve,) = fetcher.getReserves(tokenIn, tokenOut);
+
+            if (reserve > maxReserve && reserve > 0) {
+                maxReserve = reserve;
+                bestFetcher = address(fetcher);
+            }
+        }
+    }
+
+    function _sweetSpotAlgo(uint256 volume, uint256 reserves, uint256 effectiveGas) public pure returns (uint256 sweetSpot) {
+        sweetSpot = volume / (sqrt(reserves * effectiveGas));
+    }
+
+    // babylonian
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
 
 }
--- src/interfaces/IReserveFetcher.sol
+++ src/interfaces/IReserveFetcher.sol
@@ -3,4 +3,4 @@ pragma solidity ^0.8.0;
 
 interface IReserveFetcher {
     function getReserves(address tokenA, address tokenB) external view returns (uint256 reserveA, uint256 reserveB);
-}
+} 
