23c8c0a88bfd6155b0adc89ff6364c1c499a9b8a
immaxkent <zmkd@solacelabs.org>
2025-05-08 00:39:09 +0100

    feat(contracts): creating DEX interfaces for trade execution
    summary: each DEX has protocol specific signatures to trade execution functionalities. This commit features interfaces for 5 popular 1SL starter DEXs, as well as an updated IUniversalDexInterface

--- src/interfaces/IUniversalDexInterface.sol
+++ src/interfaces/IUniversalDexInterface.sol
@@ -1,6 +1,27 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 interface IUniversalDexInterface {
     function getReserves(address tokenA, address tokenB) external view returns (uint256 reserveA, uint256 reserveB);
+    function getPoolAddress(address tokenIn, address tokenOut) external view returns (address pool);
+    
+    function executeSwap(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address recipient,
+        bytes calldata data
+    ) external returns (uint256 amountOut);
+    
+    function getSwapData(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address recipient
+    ) external view returns (bytes memory swapData);
+    
+    function getDexType() external pure returns (string memory);
+    function getDexVersion() external pure returns (string memory);
 }
