e7fa827793c9de72ef206a228f16c9a014d861f8
immaxkent <zmkd@solacelabs.org>
2025-05-09 23:59:01 +0100

    feat(contracts): implementing basic/draft TWAP functionalities
    summary: in order to effectively provide a gas parameter to the sweet spot algorithm, we use time weighted averages of previously executed trades.
    
    N.B.
    1. this algorithm doesnt feature checks for array lengths (will hit out of bounds in first deployments)
    2. this algorithm doesn't convert to dollars, which is an essential requisite for the designed algorithm
    3. this algorithm doesn't ensure the limits sit between 0.1 -> 1, and thus we shuold expect the algorithm to break on current design.
    
    as such this is NOT PRODUCTION READY but rather a draft

--- src/Core.sol
+++ src/Core.sol
@@ -6,17 +6,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./StreamDaemon.sol";
 import "./Executor.sol";
 
-contract Core is Ownable/*, UUPSUpgradeable */ {
+contract Core is Ownable /*, UUPSUpgradeable */ {
     StreamDaemon public immutable streamDaemon;
     Executor public immutable executor;
-    
+
     // DEX router addresses
     address public immutable uniswapV2Router;
     address public immutable sushiswapRouter;
     address public immutable uniswapV3Router;
     address public immutable balancerVault;
     address public immutable curvePool;
-    
+
+    // Gas recording
+    uint256 private startGas;
+    uint256 public lastGasUsed;
+    uint256 public lastGasPrice;
+    uint256 public lastGasCost;
+    uint256[] public TWAP_GAS_COST;
+
     constructor(
         address _streamDaemon,
         address _executor,
@@ -37,81 +44,105 @@ contract Core is Ownable/*, UUPSUpgradeable */ {
 
     // function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    function initiateGasRecord() public {}
-    function closeGasRecord() public {}
-    
-    function executeTrade(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address recipient
-    ) external returns (uint256 amountOut) {
+    function initiateGasRecord() public {
+        startGas = gasleft();
+    }
+
+    function closeGasRecord() public {
+        uint256 gasUsed = startGas - gasleft();
+        lastGasUsed = gasUsed;
+        lastGasPrice = tx.gasprice;
+        lastGasCost = gasUsed * tx.gasprice;
+        TWAP_GAS_COST.push(lastGasCost);
+    }
+
+    function readTWAPGasCost(uint8 delta) public view returns (uint256) {
+        uint256 totalGasCost = 0;
+        for (uint256 i = 0; i < delta; i++) {
+            totalGasCost += TWAP_GAS_COST[i];
+        }
+        return totalGasCost / TWAP_GAS_COST.length;
+    }
+
+    function executeTrade() public {
+        /**
+         * this should take a trade stored in a queue,
+         * execute it via executeStream(), returning the amount settled
+         * and thereafter update the trade's metadata.
+         * 
+         * rightful considerations must be given to:
+         * - the trade's realised slippage
+         * - the trade's realised gas cost
+         * - the trade's target amountOut vs realised amountOut
+         * - fees
+         */
+    }
+
+    function executeStream(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOutMin, address recipient)
+        external
+        returns (uint256 amountOut)
+    {
         initiateGasRecord();
         // Get best DEX and number of streams from StreamDaemon
         (uint256 numStreams, address bestDex) = streamDaemon.evaluateSweetSpotAndDex(
             tokenIn,
             tokenOut,
             amountIn,
-            1 // TODO: Calculate effective gas
+            readTWAPGasCost(10) // placeholder delta. need optimisation @audit
         );
-        
+
         // Calculate amount per stream
-        uint256 amountPerStream = amountIn / numStreams;
-        
-        // Execute trades across streams
-        for (uint256 i = 0; i < numStreams; i++) {
-            uint256 currentAmount = i == numStreams - 1 ? amountIn - (amountPerStream * i) : amountPerStream;
-            
-            // Execute trade on best DEX
-            if (bestDex == uniswapV2Router) {
-                (bool success, bytes memory result) = address(executor).delegatecall(
-                    abi.encodeWithSelector(
-                        Executor.executeUniswapV2Trade.selector,
-                        uniswapV2Router,
-                        tokenIn,
-                        tokenOut,
-                        currentAmount,
-                        amountOutMin * currentAmount / amountIn,
-                        recipient
-                    )
-                );
-                require(success, "UniswapV2 trade failed");
-                amountOut += abi.decode(result, (uint256));
-            } else if (bestDex == uniswapV3Router) {
-                (bool success, bytes memory result) = address(executor).delegatecall(
-                    abi.encodeWithSelector(
-                        Executor.executeUniswapV3Trade.selector,
-                        uniswapV3Router,
-                        tokenIn,
-                        tokenOut,
-                        currentAmount,
-                        amountOutMin * currentAmount / amountIn,
-                        recipient,
-                        3000 // default fee tier
-                    )
-                );
-                require(success, "UniswapV3 trade failed");
-                amountOut += abi.decode(result, (uint256));
-            } else if (bestDex == balancerVault) {
-                // TODO: get poolId from StreamDaemon or registry
-                bytes32 poolId = bytes32(0);
-                (bool success, bytes memory result) = address(executor).delegatecall(
-                    abi.encodeWithSelector(
-                        Executor.executeBalancerTrade.selector,
-                        balancerVault,
-                        tokenIn,
-                        tokenOut,
-                        currentAmount,
-                        amountOutMin * currentAmount / amountIn,
-                        recipient,
-                        poolId
-                    )
-                );
-                require(success, "Balancer trade failed");
-                amountOut += abi.decode(result, (uint256));
-            }
+        uint256 currentAmount = amountIn / numStreams;
+
+        // Execute trade on best DEX
+        if (bestDex == uniswapV2Router) {
+            (bool success, bytes memory result) = address(executor).delegatecall(
+                abi.encodeWithSelector(
+                    Executor.executeUniswapV2Trade.selector,
+                    uniswapV2Router,
+                    tokenIn,
+                    tokenOut,
+                    currentAmount,
+                    amountOutMin * currentAmount / amountIn,
+                    recipient
+                )
+            );
+            require(success, "UniswapV2 trade failed");
+            amountOut += abi.decode(result, (uint256));
+        } else if (bestDex == uniswapV3Router) {
+            (bool success, bytes memory result) = address(executor).delegatecall(
+                abi.encodeWithSelector(
+                    Executor.executeUniswapV3Trade.selector,
+                    uniswapV3Router,
+                    tokenIn,
+                    tokenOut,
+                    currentAmount,
+                    amountOutMin * currentAmount / amountIn,
+                    recipient,
+                    3000 // default fee tier
+                )
+            );
+            require(success, "UniswapV3 trade failed");
+            amountOut += abi.decode(result, (uint256));
+        } else if (bestDex == balancerVault) {
+            // TODO: get poolId from StreamDaemon or registry
+            bytes32 poolId = bytes32(0);
+            (bool success, bytes memory result) = address(executor).delegatecall(
+                abi.encodeWithSelector(
+                    Executor.executeBalancerTrade.selector,
+                    balancerVault,
+                    tokenIn,
+                    tokenOut,
+                    currentAmount,
+                    amountOutMin * currentAmount / amountIn,
+                    recipient,
+                    poolId
+                )
+            );
+            require(success, "Balancer trade failed");
+            amountOut += abi.decode(result, (uint256));
         }
+
         closeGasRecord();
 
         return amountOut;
