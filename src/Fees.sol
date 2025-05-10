ca6d4cde8f06ca43299fdd846cf3c57169f20637
immaxkent <zmkd@solacelabs.org>
2025-05-10 18:41:03 +0100

    fix(contracts): removing unneeded comments

--- src/Fees.sol
+++ src/Fees.sol
@@ -5,29 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Fees is Ownable{
-    /**
-     * this contract is meant to collect fees on behalf of Bots (maintainters of the protocol)
-     * 
-     * each time a tx is executed, 10BPS are taken from amountOut (realised trade output) and stored 
-     * as a balance in this contract
-     * 
-     * as such, we erequire a mixture of state and fucntions:
-     * 
-     * INHERITANCE
-     * 
-     * must be able to recieve tokens
-     * 
-     * STATE
-     * 
-     * balances mapping
-     * 
-     * FUNCTIONS
-     * 
-     * (fallback and recieve functions may be implemented to be defined with logic that calls: )
-     * takeFees(bot)
-     * 
-     * withdrawFees (not automatically called in first implementation, but automatically called when transactions are batched)
-     */
 
     constructor() Ownable(msg.sender) {}
 
