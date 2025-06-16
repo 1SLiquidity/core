4803dbee82f6eac56f5b0b84b15aa12f1ace0a7c
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-06-16 10:45:29 +0500

    fix: switch tokens

--- frontend/app/components/home/SELSection/swapBox/index.tsx
+++ frontend/app/components/home/SELSection/swapBox/index.tsx
@@ -3,26 +3,34 @@ import { useState } from 'react'
 
 type Props = {
   active: boolean
+  disabled?: boolean
 }
 
-const SwapBox: React.FC<Props> = ({ active = false }) => {
+const SwapBox: React.FC<Props> = ({ active = false, disabled = false }) => {
   return (
     <div className="w-16 h-16 overflow-hidden flex gap-4 p-2 group">
       <div
-        className={`w-full h-full rounded-[4px] flex justify-center bg-neutral-800 items-center p-1 group-hover:bg-gradient-to-r from-[#071310] to-[#062118]
-          `}
+        className={`w-full h-full rounded-[4px] flex justify-center bg-neutral-800 items-center p-1 ${
+          disabled
+            ? 'opacity-50'
+            : 'group-hover:bg-gradient-to-r from-[#071310] to-[#062118]'
+        }`}
       >
         <Image
           src={'/icons/swap-arrows.svg'}
           alt="swap"
-          className="-rotate-45 w-5 group-hover:hidden block"
+          className={`-rotate-45 w-5 block ${
+            disabled ? 'block' : 'block group-hover:hidden'
+          }`}
           width={1000}
           height={1000}
         />
         <Image
           src={'/icons/swap-arrows-green.svg'}
           alt="swap"
-          className="-rotate-45 w-5 group-hover:block hidden"
+          className={`-rotate-45 w-5 block ${
+            disabled ? 'hidden' : 'hidden group-hover:block'
+          }`}
           width={1000}
           height={1000}
         />
