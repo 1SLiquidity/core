23abab40ddb9cfa3f9a36078194347fe8a9bbada
gray-guy <mshehroz99@gmail.com>
2025-06-25 19:04:07 +0300

    slippage savings

--- keeper/src/tests/run-gas-tests.ts
+++ keeper/src/tests/run-gas-tests.ts
@@ -1,91 +1,59 @@
-import { testGasCalculations } from './gas-calculations';
+import { testGasCalculations, testSlippageSavings } from './gas-calculations';
 
 async function runTests() {
   try {
 
-    console.log('\nTesting USDC/WBTC pair:');
-    const wethUsdcResult = await testGasCalculations(
-      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
-      '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
-      '1000' // 1000 USDC
-    );
-    console.log('USDC/WBTC Results:', {
-      botGasLimit: wethUsdcResult.botGasLimit.toString(),
-      streamCount: wethUsdcResult.streamCount,
-    });
-
-
-    // Test with WETH/USDC pair
-    // console.log('\nTesting WETH/USDC pair:');
+    // console.log('\nTesting USDC/WBTC pair:');
     // const wethUsdcResult = await testGasCalculations(
-    //   '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
     //   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
-    //   '33.0' // 33 ETH
+    //   '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
+    //   '10000' // 10000 USDC
     // );
-    // console.log('WETH/USDC Results:', {
+    // console.log('USDC/WBTC Results:', {
     //   botGasLimit: wethUsdcResult.botGasLimit.toString(),
     //   streamCount: wethUsdcResult.streamCount,
     // });
 
-    // const usdcUsdtResult = await testGasCalculations(
-    //   '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-    //   '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-    //   '100.0' // 100 ETH
+    // console.log('\nTesting WBTC/USDC pair:');
+    // const wethUsdcResult2 = await testGasCalculations(
+    //   '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
+    //   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
+    //   '1' // 1 WBTC
     // );
-    // console.log('USDC/USDT Results:', {
-    //   botGasLimit: usdcUsdtResult.botGasLimit.toString(),
-    //   streamCount: usdcUsdtResult.streamCount,
+    // console.log('WBTC/USDC Results:', {
+    //   botGasLimit: wethUsdcResult2.botGasLimit.toString(),
+    //   streamCount: wethUsdcResult2.streamCount,
     // });
 
-    // // Test with different trade volumes
-    // console.log('\nTesting different trade volumes:');
-    // const volumes = ['0.1', '1.0', '10.0', '100.0'];
-    // for (const volume of volumes) {
-    //   const result = await testGasCalculations(
-    //     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
-    //     '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
-    //     volume
-    //   );
-    //   console.log(`\nVolume: ${volume} ETH`);
-    //   console.log('Results:', {
-    //     botGasLimit: result.botGasLimit.toString(),
-    //     gasPrice: result.gasPrice.toString(),
-    //     streamCount: result.streamCount,
-    //     tradeVolume: result.tradeVolume.toString()
-    //   });
-    // }
+    // console.log('\nTesting PEPE/WBTC pair:');
+    // const wethUsdcResult3 = await testGasCalculations(
+    //   '0x6982508145454Ce325dDbE47a25d4ec3d2311933', // PEPE
+    //   '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
+    //   '10000' // 10000 PEPE
+    // );
+    // console.log('PEPE/WBTC Results:', {
+    //   botGasLimit: wethUsdcResult3.botGasLimit.toString(),
+    //   streamCount: wethUsdcResult3.streamCount,
+    // });
 
-    // // Test with different token pairs
-    // console.log('\nTesting different token pairs:');
-    // const pairs = [
-    //   {
-    //     name: 'WETH/USDT',
-    //     tokenA: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
-    //     tokenB: '0xdAC17F958D2ee523a2206206994597C13D831ec7', // USDT
-    //     volume: '1.0'
-    //   },
-    //   {
-    //     name: 'WETH/DAI',
-    //     tokenA: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
-    //     tokenB: '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
-    //     volume: '1.0'
-    //   }
-    // ];
+    // console.log('\nTesting WETH/USDC pair:');
+    // const wethUsdcResult4 = await testGasCalculations(
+    //   '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
+    //   '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
+    //   '10' // 10 WETH
+    // );
+    // console.log('WETH/USDC Results:', {
+    //   botGasLimit: wethUsdcResult4.botGasLimit.toString(),
+    //   streamCount: wethUsdcResult4.streamCount,
+    // });
 
-    // for (const pair of pairs) {
-    //   console.log(`\nTesting ${pair.name}:`);
-    //   const result = await testGasCalculations(
-    //     pair.tokenA,
-    //     pair.tokenB,
-    //     pair.volume
-    //   );
-    //   console.log('Results:', {
-    //     botGasLimit: result.botGasLimit.toString(),
-    //     gasPrice: result.gasPrice.toString(),
-    //     streamCount: result.streamCount,
-    //     tradeVolume: result.tradeVolume.toString()
-    //   });
-    // }
+    console.log('\nTesting slippage savings for USDC/WBTC pair:');
+    const slippageSavings = await testSlippageSavings(
+      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
+      '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
+      '100000' // 10000 USDC
+    );
+    console.log('Slippage savings:', slippageSavings);
 
   } catch (error) {
     console.error('Error running tests:', error);
