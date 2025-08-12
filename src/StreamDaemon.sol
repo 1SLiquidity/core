0c049f0fc275a4ec16c6317372a32dd5758ddc6f
drikssy <cedricnga2@gmail.com>
2025-08-13 00:27:10 +0200

    refactor: remove unused console import from StreamDaemon
    - Remove unnecessary forge-std/console.sol import
    - Clean up unused debugging dependency

--- src/StreamDaemon.sol
+++ src/StreamDaemon.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "forge-std/console.sol";
-
 import { IUniversalDexInterface } from "./interfaces/IUniversalDexInterface.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
